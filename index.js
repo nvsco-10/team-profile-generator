d535246f64abe18644ae37552f9fa0c412e20112
Neema Velasco <neemavelasco@gmail.com>
2022-03-28 18:09:02 -0500

    added createManager function using inquirer prompt

--- index.js
+++ index.js
@@ -1,2 +1,37 @@
 const inquirer = require('inquirer');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+async function createManager() {
+    const questions = [
+        {
+            type: "input",
+            name: "name",
+            message: "What is the team manager's name?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is the team manager's employee id?"
+        },
+        {
+            type: "email",
+            name: "email",
+            message: "What is the team manager's email address?"
+        },
+        {
+            type: "input",
+            name: "officenum",
+            message: "What is your the team manager's office number?"
+        },
+
+    ];
+
+    const managerProfile = await inquirer.prompt(questions);
+    
+    console.log(managerProfile)
+    return managerProfile;
+
+}
 
