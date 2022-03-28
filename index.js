8e6a76f39969609578ff255eb1a4eb6f47f3d526
Neema Velasco <neemavelasco@gmail.com>
2022-03-28 18:13:39 -0500

    added createEngineer function

--- index.js
+++ index.js
@@ -35,3 +35,35 @@ async function createManager() {
 
 }
 
+async function createEngineer() {
+    const questions = [
+        {
+            type: "input",
+            name: "name",
+            message: "What is the engineer's name?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is the engineer's employee id?"
+        },
+        {
+            type: "email",
+            name: "email",
+            message: "What is the engineer's email address?"
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "What is the engineer's GitHub username?"
+        },
+
+    ];
+
+    const engineerProfile = await inquirer.prompt(questions);
+    
+    console.log(engineerProfile)
+    return engineerProfile;
+}
+
+createEngineer();
